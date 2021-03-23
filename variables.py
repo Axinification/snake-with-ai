447d4c143a797fa8fd4e775c196df0ccdb918ccf
Axinification <Alekwit@gmail.com>
2021-03-23 14:22:55 +0100

    Gamma incrementation fix

--- variables.py
+++ variables.py
@@ -21,13 +21,13 @@ EPSILON_DELTA = 80 # 0 randomness after x games
 IS_INCREMENTING = True # On/Off option for gamma incrementing
 GAMMA = 0.9 # Has to be less than 1. Lower discount rate strives for quick rewards and higher for the long term ones
 GAMMA_LOW = 0.1 # Used for incrementing gamma over time // Set IS_INCREMENTING to True for it to work
-GAMMA_INCREMENT = 0.01 # 
+GAMMA_INCREMENT = 0.002 # Amount of gamma increment
 #Reward system
 REWARD = 10 # Both positive and negative // also minimal reward on time penalty
 REWARD_MULTIPLIER = 3 # Multiplies positive reward
-TIME_PENALTY = 0.01 # This times frames after collecting apple or start will be substracted from apple reward
+TIME_PENALTY = 0.005 # This times frames after collecting apple or start will be substracted from apple reward
 
 LOOP_TIME = 100 # Per snake segment
 
 CHECKPOINT_PATH = "./checkpoint"
-LOAD = True
+LOAD = False
